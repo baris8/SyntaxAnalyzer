533c9e5341fb660cbb87b67ac2364f8fd4af9479
Baris <uectas.ba@gmail.com>
2018-06-14 19:25:50 +0200

    First Half Done

--- src/syntaxanalyzer/JackTokenizer.java
+++ src/syntaxanalyzer/JackTokenizer.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 public class JackTokenizer {
     private File file;
     private String curToke;
+    private String out;
     
     public JackTokenizer(File f){
         file = f;
+        out = "<tokens>\n";
     }
     
     public void tokenize() throws FileNotFoundException{
@@ -19,57 +21,89 @@ public class JackTokenizer {
             current = scanner.nextLine();
             if(!current.equals("")){
                 current = current.replaceAll("//.*", "").trim();
+                String[] curWords = current.split(" ");
+                for(int i = 0; i < curWords.length; i++){
+                    String s = "";
+                    int j = 0;
+                    curWords[i] = curWords[i].replaceAll("\t", "");
+
+                    while(j < curWords[i].length()){
+                        if(Character.isDigit(curWords[i].charAt(j))){
+                            out += "<integerConstant> ";
+                            while(Character.isDigit(curWords[i].charAt(j))){
+                                out += curWords[i].charAt(j);
+                                j++;
+                            }
+                            out += " </integerConstant>\n";
+                            out += tokenType(";");
+                        }else if(curWords[i].charAt(j) == ';' ||  curWords[i].charAt(j) == '.' || curWords[i].charAt(j) == '(' ||
+                            curWords[i].charAt(j) == ')' || curWords[i].charAt(j) == ',' || curWords[i].charAt(j) == ']' || curWords[i].charAt(j) == '['){
+                            out += tokenType(s);
+                            s = "";
+                            out += tokenType(""+curWords[i].charAt(j));
+                            curWords[i] = curWords[i].substring(j);
+                            j = 0;
+                        }else if(curWords[i].charAt(j) == '\"'){
+                            out += tokenType(s);
+                            out += "<stringConstant>";
+                            curWords[i] = curWords[i].substring(j+1);
+                            int z = i;
+
+                            while(!curWords[z].contains("\"") && z < curWords.length){
+                                out += " " + curWords[z];
+                                z++;
+                            }
+                            System.out.println(curWords[z]);
+                            out += " </stringConstant>\n";
+                            out += tokenType(")");
+                            i = z;
+                            s = "";
+                        }else{
+                            s += curWords[i].charAt(j);
+                        }
+                        j++;
+                    }
+                    out += tokenType(s);
+                }
             }
         }
+        out += "</tokens>";
     }
-        
+      
+    
+    
     public String tokenType(String curToke){
         String out = "";
         switch(curToke){
-            case "class":
-            case "constructor":           
-            case "function": 
-            case "method":
-            case "field": 
-            case "static":
-            case "var":
-            case "int": 
-            case "char": 
-            case "boolean":
-            case "void": 
-            case "true":
-            case "false":
-            case "null": 
-            case "this": 
-            case "let": 
-            case "do":
-            case "if": 
-            case "else": 
-            case "while":
-            case "return":
-                out = "KEYWORD"; break;
-            case "{": 
-            case "}":
-            case "(":
-            case ")": 
-            case "[": 
-            case "]": 
-            case ".":
-            case ",": 
-            case ";": 
-            case "+":
-            case "-":
-            case "*": 
-            case "/":
-            case "&":
-            case "|": 
-            case "<": 
-            case ">": 
-            case "=":
-            case "~":
-                out = "SYMBOL"; break;
+            case "class": case "constructor": case "function": 
+            case "method": case "field": case "static": case "var":
+            case "int": case "char": case "boolean":
+            case "void": case "true": case "false":
+            case "null": case "this": case "let": 
+            case "do": case "if": case "else": 
+            case "while": case "return":
+                                    out = "<keyword> " + curToke + " </keyword>\n"; break;
+            case "{": case "}": case "(": case ")": 
+            case "[": case "]": case ".": case ",": 
+            case ";": case "+": case "-": case "*": 
+            case "/": case "&": case "|": 
+            case "=": case "~":
+                                    out = "<symbol> " + curToke + " </symbol>\n"; break;
+            case ">":   out = "<symbol> &gt </symbol>\n"; break;
+            case "<":   out = "<symbol> &lt </symbol>\n"; break;
+        }
+        if(out.equals("") && curToke.length() > 0){
+            out = "<identifier> " + curToke + " </identifier>\n";
         }
         return out;
+    }
+    
+    public void strContainsSmth(String token){
+        if(token.contains(";")){
+            token = token.split(";")[0];
+            tokenType(token);
+            tokenType(";");
+        }
     }
     
     public String keyWord(String curToke){
@@ -100,10 +134,7 @@ public class JackTokenizer {
         return out;
     }
 
-    public String symbol(char curToke){
-        char out;
-        switch(curToke){
-            
-        }
+    public String getOut(){
+        return out;
     }
 }
--- src/syntaxanalyzer/SyntaxAnalyzer.java
+++ src/syntaxanalyzer/SyntaxAnalyzer.java
@@ -1,11 +1,6 @@
 package syntaxanalyzer;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class SyntaxAnalyzer extends Application {
