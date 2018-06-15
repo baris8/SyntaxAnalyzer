ec185c0a23724b5a5af8568e4be151ea1619018d
Baris <uectas.ba@gmail.com>
2018-06-15 12:40:25 +0200

    Tokenizing Done

--- src/syntaxanalyzer/JackTokenizer.java
+++ src/syntaxanalyzer/JackTokenizer.java
@@ -21,56 +21,36 @@ public class JackTokenizer {
             current = scanner.nextLine();
             if(!current.equals("")){
                 current = current.replaceAll("//.*", "").trim();
-                String[] curWords = current.split(" ");
-                for(int i = 0; i < curWords.length; i++){
-                    String s = "";
-                    int j = 0;
-                    curWords[i] = curWords[i].replaceAll("\t", "");
-
-                    while(j < curWords[i].length()){
-                        if(Character.isDigit(curWords[i].charAt(j))){
-                            out += "<integerConstant> ";
-                            while(Character.isDigit(curWords[i].charAt(j))){
-                                out += curWords[i].charAt(j);
-                                j++;
-                            }
-                            out += " </integerConstant>\n";
-                            out += tokenType(";");
-                        }else if(curWords[i].charAt(j) == ';' ||  curWords[i].charAt(j) == '.' || curWords[i].charAt(j) == '(' ||
-                            curWords[i].charAt(j) == ')' || curWords[i].charAt(j) == ',' || curWords[i].charAt(j) == ']' || curWords[i].charAt(j) == '['){
-                            out += tokenType(s);
-                            s = "";
-                            out += tokenType(""+curWords[i].charAt(j));
-                            curWords[i] = curWords[i].substring(j);
-                            j = 0;
-                        }else if(curWords[i].charAt(j) == '\"'){
-                            out += tokenType(s);
-                            out += "<stringConstant>";
-                            curWords[i] = curWords[i].substring(j+1);
-                            int z = i;
-
-                            while(!curWords[z].contains("\"") && z < curWords.length){
-                                out += " " + curWords[z];
-                                z++;
-                            }
-                            System.out.println(curWords[z]);
-                            out += " </stringConstant>\n";
-                            out += tokenType(")");
-                            i = z;
-                            s = "";
-                        }else{
-                            s += curWords[i].charAt(j);
+                current = current.replaceAll("//*/*.*/*/", "").trim();
+                
+                String s = "";
+                for(int i = 0; i < current.length(); i++){
+                    if(current.charAt(i) == ';' ||  current.charAt(i) == '.' || current.charAt(i) == '(' ||
+                        current.charAt(i) == ')' || current.charAt(i) == ',' || current.charAt(i) == ']' || current.charAt(i) == '['){
+                        out += tokenType(s);
+                        s = "";
+                        out += tokenType(""+ current.charAt(i));
+                    }else if(current.charAt(i) == ' '){
+                        out += tokenType(s);
+                        s = "";
+                    }else if(current.charAt(i) == '\"'){
+                        s += "$";
+                        i++;
+                        while(current.charAt(i) != '\"'){
+                            s += current.charAt(i);
+                            i++;
                         }
-                        j++;
+                        out += tokenType(s);
+                        s = "";
+                    }else{
+                        s += current.charAt(i);
                     }
-                    out += tokenType(s);
                 }
+                out += tokenType(s);
             }
         }
-        out += "</tokens>";
+        out += "</tokens>\n";
     }
-      
-    
     
     public String tokenType(String curToke){
         String out = "";
@@ -89,51 +69,29 @@ public class JackTokenizer {
             case "/": case "&": case "|": 
             case "=": case "~":
                                     out = "<symbol> " + curToke + " </symbol>\n"; break;
-            case ">":   out = "<symbol> &gt </symbol>\n"; break;
-            case "<":   out = "<symbol> &lt </symbol>\n"; break;
+            case ">":   out = "<symbol> &gt; </symbol>\n"; break;
+            case "<":   out = "<symbol> &lt; </symbol>\n"; break;
         }
-        if(out.equals("") && curToke.length() > 0){
+        if(out.equals("") && curToke.length() > 0 && curToke.charAt(0) == '$'){
+            out = "<stringConstant> " + curToke.substring(1) + " </stringConstant>\n";
+        }
+        if(out.equals("") && curToke.length() > 0 && !Character.isDigit(curToke.charAt(0))){
             out = "<identifier> " + curToke + " </identifier>\n";
         }
-        return out;
-    }
-    
-    public void strContainsSmth(String token){
-        if(token.contains(";")){
-            token = token.split(";")[0];
-            tokenType(token);
-            tokenType(";");
-        }
-    }
-    
-    public String keyWord(String curToke){
-        String out = "";
-        switch(curToke){
-            case "class": out = "CLASS"; break;
-            case "method": out = "METHOD"; break;
-            case "function": out = "FUNCTION"; break;
-            case "constructor": out = "CONSTRUCTOR"; break;
-            case "int": out = "INT"; break;
-            case "boolean": out = "BOOLEAN"; break;
-            case "char": out = "CHAR"; break;
-            case "void": out = "VOID"; break;
-            case "var": out = "VAR"; break;
-            case "static": out = "STATIC"; break;
-            case "field": out = "FIELD"; break;
-            case "let": out = "LET"; break;
-            case "do": out = "DO"; break;
-            case "if": out = "IF"; break;
-            case "else": out = "ELSE"; break;
-            case "while": out = "WHILE"; break;
-            case "return": out = "RETURN"; break;
-            case "true": out = "TRUE"; break;
-            case "false": out = "FALSE"; break;
-            case "null": out = "NULL"; break;
-            case "this": out = "THIS"; break;
+        if(curToke.length() > 0 && Character.isDigit(curToke.charAt(0))){
+            boolean integer = true;
+            try {
+                Integer num = Integer.parseInt(curToke);
+            } catch (NumberFormatException e) {
+                integer = false;
+            }
+            if(integer){
+                out = "<integerConstant> " + curToke + " </integerConstant>\n";
+            }
         }
         return out;
     }
-
+    
     public String getOut(){
         return out;
     }
--- src/syntaxanalyzer/SyntaxAnalyzer.java
+++ src/syntaxanalyzer/SyntaxAnalyzer.java
@@ -1,15 +1,91 @@
 package syntaxanalyzer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class SyntaxAnalyzer extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        GridPane root = new GridPane();
+        root.setHgap(10);
+        root.setVgap(10);
+        
+        //Szene erstellen
+        String s = "";
+        Scene scene = new Scene(root, 900, 600);
+        Label vm = new Label("VM-Code");
+        Label assembler = new Label("Assembler-Code");
+        Button saveButton = new Button("Save as ASM File");
+        
+        TextArea vmText = new TextArea();
+        TextArea asmText = new TextArea();
+        vmText.setPrefHeight(500);
+        
+        root.add(vm, 0, 0);
+        root.add(assembler, 1, 0);
+        root.add(vmText, 0, 1);
+        root.add(asmText, 1, 1);
+        root.add(saveButton, 1, 2);
+        
+        //Datei angeben
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Open VM File");
+        fc.getExtensionFilters().addAll(new ExtensionFilter("Jack File", "*.jack"));
+        File f = fc.showOpenDialog(primaryStage);
 
+        //vmFile anzeigen 
+        String ao = "";
+        Scanner scanner = new Scanner(f);
+        while(scanner.hasNext()){
+            String line = scanner.nextLine();
+            ao += line + "\n";
+        }
+        vmText.setText(ao);
+        
+        JackTokenizer jt = new JackTokenizer(f);
+        jt.tokenize();
+        asmText.setText(jt.getOut());
+        
+        saveButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                PrintWriter writer;
+                try {
+                    String name = f.getName().replace(".jack", "T");
+                    writer = new PrintWriter(name + ".xml");
+                    writer.println(jt.getOut());
+                    writer.close();
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(SyntaxAnalyzer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        
+        primaryStage.setTitle("Project 10 - by Baris Üctas");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    
+    
+    
     public static void main(String[] args) {
         launch(args);
     }
